0b555c706b2ee9c6e5d823db1c581ca5b059a585
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-10-17 23:58:06 -0300

    Create api models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=3)
+
+class Indicator(models.Model):
+    name = models.CharField(max_length=70)
+    code = models.CharField(max_length=20)
+
+class Value(models.Model):
+    value = models.CharField(max_length=255)
+    year = models.CharField(max_length=4)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    indicator = models.ForeignKey(Indicator, on_delete=models.CASCADE)
