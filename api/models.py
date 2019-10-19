79d3e8b0c53caa97a95a6a3efc84c7e02f2a4a2d
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-10-19 01:18:14 -0300

    Modify models to the reporting database architecture

--- api/models.py
+++ api/models.py
@@ -1,18 +1,9 @@
 from django.db import models
 
-
-class Country(models.Model):
-    name = models.CharField(max_length=50)
-    code = models.CharField(max_length=3)
-
-
 class Indicator(models.Model):
-    name = models.CharField(max_length=70)
-    code = models.CharField(max_length=20)
-
-
-class Value(models.Model):
-    value = models.CharField(max_length=255)
+    indicator_name = models.CharField(max_length=70)
+    indicator_code = models.CharField(max_length=20)
+    country_name = models.CharField(max_length=50)
+    country_code = models.CharField(max_length=3)
+    value = models.CharField(max_length=255, default='')
     year = models.CharField(max_length=4)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    indicator = models.ForeignKey(Indicator, on_delete=models.CASCADE)
